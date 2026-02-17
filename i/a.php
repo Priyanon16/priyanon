b9d8057ebbc22e143c2562ae6ede97a737db7042
Priyanon16 <66010912608@msu.ac.th>
2026-02-17 18:21:14 +0700

    a

--- i/a.php
+++ i/a.php
@@ -8,5 +8,30 @@
 <body>
 
 <h1>งาน i -- ปรียานนท์ กรุตนิด (มินนี่)</h1>
+
+<?php
+include_once("connectdb.php");
+$sql = SELECT * FROM `regions`
+$rs = mysqli_query($conn, $sql);
+while ($data = mysqli_fetch_array($rs)){
+    echo $data['r_id']. "<br>";
+    echo $data['r_name']. "<hr>";
+}
+
+mysqli_close($conn);
+?>
+
+<table border="1">
+    <tr>
+        <hr>รหัสภาค</hr>
+        <hr>ชื่อภาค</hr>
+    </tr>
+
+    <tr>
+        <hr>xxx</hr>
+        <hr>xxx</hr>
+    </tr>
+
+</table>
 </body>
 </html>
