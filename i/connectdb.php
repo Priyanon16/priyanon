9090399f2d200e1f4f6b4d8fbf4fe1f8f50c0830
Priyanon16 <66010912608@msu.ac.th>
2026-02-17 19:08:11 +0700

    a

--- i/connectdb.php
+++ i/connectdb.php
@@ -1,8 +1,8 @@
 <?php
 
     $host = "localhost";
-	   $user = "root";
-	   $pwd = "";
+	   $user = "admin_man";
+	   $pwd = "66010914015";
 	   $db = "2608db";
        $conn = mysqli_connect($host, $user, $pwd, $db) or die ("เชื่อมต่อฐานข้อมูลไม่ได้"); //เชื่อมฐานข้อมูลดาต้าเบส
 	   mysqli_query($conn, "SET NAMES utf8"); //อ่านหรือบันทึกข้อมูลภาษาไทยได้
