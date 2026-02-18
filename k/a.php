fa7eac368088b4d3b567a55e1cf851aa1c0224c6
Priyanon16 <66010912608@msu.ac.th>
2026-02-18 16:48:33 +0700

    a

---
+++ k/a.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>66010912608 ปรียานนท์ กรุตนิด (มินนี่)</title>
+</head>
+
+<body>
+
+
+<h1>งาน k - 66010912608 ปรียานนท์ กรุตนิด (มินนี่)</h1>
+<br>
+
+<button type="button" onclick="window.open('images/1.jpg')">ปุ่มสีเขียว</button>
+
+<button onclick="window.open('image2.jpg')">ปุ่มสีส้ม</button>
+
+				
+          
+</body>
+</html>
