c9a1e814f675cf80c0721318621f02de599f3a76
Priyanon16 <66010912608@msu.ac.th>
2026-02-18 13:31:57 +0700

    a

---
+++ j/a.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ปรียานนท์ กรุตนิด (มินนี่)</title>
+</head>
+
+<body>
+
+
+<h1>ปรียานนท์ กรุตนิด (มินนี่)</h1>
+<br>
+
+<div class="col-2">
+      <img src="1.jpg" class="img-fluid">
+</div>
+
+				
+          
+</body>
+</html>
